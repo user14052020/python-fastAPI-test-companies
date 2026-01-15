7eba2280eebcc117937526fc8267bfad03f3f036
Vadim Maharram <vadimmaharram@happys-MacBook-Pro.local>
2026-01-15 14:00:06 +0500

    main.py API_KEY deleted

--- app/main.py
+++ app/main.py
@@ -1,34 +1,7 @@
-import os
-from fastapi import FastAPI, Security, HTTPException
-from fastapi.security.api_key import APIKeyHeader
-from starlette.status import HTTP_403_FORBIDDEN
+from fastapi import FastAPI, Security
 
 from app.api import organizations, buildings, activities
-
-# =========================
-# API KEY AUTH (OpenAPI)
-# =========================
-
-API_KEY_NAME = "X-API-Key"
-
-# Swagger поймёт это как security scheme и даст кнопку Authorize
-api_key_scheme = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
-
-
-def api_key_auth(api_key: str = Security(api_key_scheme)) -> str:
-    """
-    Проверка API ключа.
-    Ключ хранится в переменной окружения API_KEY (например в .env).
-    """
-    expected_key = os.getenv("API_KEY", "SECRET_API_KEY")
-
-    if api_key == expected_key:
-        return api_key
-
-    raise HTTPException(
-        status_code=HTTP_403_FORBIDDEN,
-        detail="Invalid API Key",
-    )
+from app.core.security import api_key_auth
 
 
 # =========================
